007d340de261157e01f4bc8cb7ea27f1245f4c16
Mark Ng <stngad@connect.ust.hk>
2021-09-24 12:23:24 +0800

    Implemented post address

--- src/web_demo/logic.clj
+++ src/web_demo/logic.clj
@@ -1,10 +1,14 @@
 (ns web-demo.logic
-  (:require [next.jdbc :as jdbc]))
+  (:require [next.jdbc :as jdbc]
+            [next.jdbc.sql :as sql]))
 
 (defn get-all-address [ds]
-  (str (jdbc/execute! ds ["select * from address"])))
+  (jdbc/execute! ds ["select * from address"]))
 
 (defn get-address-by-id [ds id]
-  (str (jdbc/execute! ds ["select * from address where id = ?" id])))
+  (jdbc/execute! ds ["select * from address where id = ?" id]))
+
+(defn post-address [ds address-detail]
+  (sql/insert! ds :address address-detail))
 
 
--- src/web_demo/routes.clj
+++ src/web_demo/routes.clj
@@ -2,17 +2,23 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [web-demo.logic :as logic]
-            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [ring.util.response :refer [response]]))
 
 (defn create-routes [ds]
  (->> (defroutes app
-        (GET "/mark" [] (response {:a "mark 1"}))
-        (GET "/" [] (logic/get-all-address ds))
-        (GET "/user/:id" [id] (str "USER ID: " id))
-        (GET "/address/:id" [id] (logic/get-address-by-id ds id))
+        ;; Address APIs
+        (context "/address" []
+          (GET "/" [] (response (logic/get-all-address ds)))
+          (POST "/" [:as {:keys [body]}]
+            (logic/post-address ds body)
+            (response {:msg "Post address successfully"}))
+          (GET "/:id" [id] (response (logic/get-address-by-id ds id))))
+
+        ;; Not Found APIs
         (route/not-found "Page Not Found"))
-      (wrap-json-response)))
+      (wrap-json-response)
+      (wrap-json-body)))
 
 (defn start [{:keys [ds] :as config}]
   (assoc config :handler (create-routes ds)))
